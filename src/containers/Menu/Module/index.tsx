ba09af70f940af0ab0c583014fca207711775c1a
Nafiz Ahmed <nafiz@mpower-social.com>
2019-11-18 17:50:28 +0600

    Add onClick handler

--- src/containers/Menu/Module/index.tsx
+++ src/containers/Menu/Module/index.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 import { Card, CardBody, CardTitle } from 'reactstrap';
-import { ModuleMenu } from '../../../store/ducks/menu';
+import { ModuleMenu, setMenuItem } from '../../../store/ducks/menu';
 
 export interface ModuleMenuItemProps {
   menuItem: ModuleMenu;
+  setMenuItemActionCreator: any;
 }
 
 class ModuleMenuItem extends React.Component<ModuleMenuItemProps> {
@@ -12,13 +14,31 @@ class ModuleMenuItem extends React.Component<ModuleMenuItemProps> {
     return (
       <div>
         <Card>
-          <CardBody>
+          <CardBody onClick={this.onClickHandler}>
             <CardTitle>{menuItem.name}</CardTitle>
           </CardBody>
         </Card>
       </div>
     );
   }
+  // tslint:disable-next-line: variable-name
+  private onClickHandler = (_event: React.MouseEvent<HTMLDivElement>) => {
+    const { menuItem } = this.props;
+    this.props.setMenuItemActionCreator(menuItem);
+  };
 }
 
-export default ModuleMenuItem;
+/** connect the component to the store */
+
+/** map props to actions */
+const mapDispatchToProps = {
+  setMenuItemActionCreator: setMenuItem,
+};
+
+/** connect clientsList to the redux store */
+const ConnectedModuleMenuItem = connect(
+  null,
+  mapDispatchToProps
+)(ModuleMenuItem);
+
+export default ConnectedModuleMenuItem;
