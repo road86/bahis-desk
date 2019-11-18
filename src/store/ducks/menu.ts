c599b61409bffafee0edb827c1d5fe78f4acf721
Nafiz Ahmed <nafiz@mpower-social.com>
2019-11-18 13:06:12 +0600

    Add setMenuItem action creator

--- src/store/ducks/menu.ts
+++ src/store/ducks/menu.ts
@@ -1,3 +1,5 @@
+import { AnyAction } from 'redux';
+
 /** The reducer name */
 export const reducerName = 'menu';
 
@@ -40,3 +42,28 @@ export interface ModuleMenu {
   img_id: number;
   children: Array<ModuleMenu | ListMenu | FormMenu>;
 }
+
+// actions
+
+/** SET_MENU_ITEM action type */
+export const SET_MENU_ITEM = 'opensrp/reducer/menu/SET_MENU_ITEM';
+
+/** interface for SET_MENU_ITEM action */
+export interface SetMenuItemAction extends AnyAction {
+  menuItem: FormMenu | ListMenu | ModuleMenu | null;
+  type: typeof SET_MENU_ITEM;
+}
+
+/** Create type for menu reducer actions */
+export type MenuActionTypes = SetMenuItemAction | AnyAction;
+
+// action creators
+
+/** set menu item action creator
+ * @param {ModuleMenu | FormMenu | ListMenu} menuItem - menuItem add to store
+ * @return {SetMenuItemAction} - an action to add menuItem to store
+ */
+export const SetMenuItem = (menuItem: ModuleMenu | FormMenu | ListMenu): SetMenuItemAction => ({
+  menuItem,
+  type: SET_MENU_ITEM,
+});
