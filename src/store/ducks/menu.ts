6b8b366a2b1cb95fe55e7aeacc850852a682b038
Nafiz Ahmed <nafiz@mpower-social.com>
2019-11-18 12:46:31 +0600

    Add Form Menu interface

--- src/store/ducks/menu.ts
+++ src/store/ducks/menu.ts
@@ -8,3 +8,17 @@ export const LIST_TYPE = 'list';
 export type LIST_TYPE = typeof LIST_TYPE;
 export const MODULE_TYPE = 'container';
 export type MODULE_TYPE = typeof MODULE_TYPE;
+
+/** interface for multi language label object */
+export interface Label {
+  [key: string]: string;
+}
+
+/** interface for form menu */
+export interface FormMenu {
+  type: FORM_TYPE;
+  name: string;
+  label: Label;
+  img_id: number;
+  form_id: number;
+}
