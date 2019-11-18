6fb547ddbf38708d0bb614f200e1ec12bb60baae
Nafiz Ahmed <nafiz@mpower-social.com>
2019-11-18 12:03:45 +0600

    Refactor and Add new db and api features

--- public/electron.js
+++ public/electron.js
@@ -4,11 +4,14 @@ const path = require('path');
 const isDev = require('electron-is-dev');
 const Database = require('better-sqlite3');
 const os = require('os');
+const fs = require('fs');
 
 // eslint-disable-next-line no-unused-vars
-const { app, BrowserWindow, ipcMain, ipcRenderer, remote, fs } = electron;
+const { app, BrowserWindow, ipcMain, ipcRenderer, remote } = electron;
 let mainWindow;
-function createWindow() {
+
+/** add the react and redux devtools to electron */
+function addDevToolsExt() {
   BrowserWindow.addDevToolsExtension(
     path.join(
       os.homedir(),
@@ -21,6 +24,31 @@ function createWindow() {
       '/.config/google-chrome/Default/Extensions/lmhkpmbekcpmknklioeibfkpmmfibljd/2.17.0_0'
     )
   );
+}
+
+/** sets up new databse. Creates tables that are required */
+function setUpNewDB() {
+  const db = new Database('foobar.db');
+  const setUpQueries = fs.readFileSync('set-up-queries.sql', 'utf8');
+  db.exec(setUpQueries);
+  db.close();
+}
+/** set up new db if not exists */
+function prepareDb() {
+  try {
+    const db = new Database('foobar.db', { fileMustExist: true });
+    db.close();
+  } catch (err) {
+    setUpNewDB();
+    // eslint-disable-next-line no-console
+    console.log('Database Setup!!!!');
+  }
+}
+
+/** creats the db and sets up the window in electron */
+function createWindow() {
+  addDevToolsExt();
+  prepareDb();
   mainWindow = new BrowserWindow({
     width: 900,
     height: 680,
@@ -36,15 +64,30 @@ function createWindow() {
     mainWindow = null;
   });
 }
+
 app.on('ready', createWindow);
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
+
 app.on('activate', () => {
   if (mainWindow === null) {
     createWindow();
+  }
+});
+
+ipcMain.on('fetch-app-definition', event => {
+  try {
+    const db = new Database('foobar.db', { fileMustExist: true });
+    // eslint-disable-next-line no-param-reassign
+    event.returnValue = db.prepare('SELECT definition from app limit 1').get().definition;
+    db.close();
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log(err);
   }
 });
 
