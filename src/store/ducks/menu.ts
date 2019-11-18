6dbae53b89ec7313dfae32cdba28c3c513195eef
Nafiz Ahmed <nafiz@mpower-social.com>
2019-11-18 12:39:07 +0600

    Add Form Menu types

---
+++ src/store/ducks/menu.ts
@@ -0,0 +1,10 @@
+/** The reducer name */
+export const reducerName = 'menu';
+
+/** Types of Menu Items */
+export const FORM_TYPE = 'form';
+export type FORM_TYPE = typeof FORM_TYPE;
+export const LIST_TYPE = 'list';
+export type LIST_TYPE = typeof LIST_TYPE;
+export const MODULE_TYPE = 'container';
+export type MODULE_TYPE = typeof MODULE_TYPE;
