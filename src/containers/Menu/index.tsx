512c4bb946b4e27793f6cac5088e5172bda05470
Nafiz Ahmed <nafiz@mpower-social.com>
2019-11-18 17:50:01 +0600

    Add back button feature

--- src/containers/Menu/index.tsx
+++ src/containers/Menu/index.tsx
@@ -1,17 +1,19 @@
 import reducerRegistry from '@onaio/redux-reducer-registry';
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Col, Row } from 'reactstrap';
+import { Button, Col, Row } from 'reactstrap';
 import { Store } from 'redux';
 import { ipcRenderer } from '../../services/ipcRenderer';
 import menuReducer, {
   FORM_TYPE,
   getCurrentMenu,
+  isPrevMenuEmpty,
   LIST_TYPE,
   MenuItem,
   MODULE_TYPE,
   reducerName as menuReducerName,
   setMenuItem,
+  setPrevMenu,
 } from '../../store/ducks/menu';
 import FormMenuItem from './Form';
 import ListMenuItem from './List';
@@ -22,7 +24,9 @@ reducerRegistry.register(menuReducerName, menuReducer);
 
 export interface MenuProps {
   setMenuItemActionCreator: any;
+  setPrevMenuActionCreator: any;
   currentMenu: MenuItem | null;
+  isBackPossible: boolean;
 }
 
 class Menu extends React.Component<MenuProps> {
@@ -34,9 +38,10 @@ class Menu extends React.Component<MenuProps> {
     }
   }
   public render() {
-    const { currentMenu } = this.props;
+    const { currentMenu, isBackPossible } = this.props;
     return (
       <div className="menu-container">
+        {isBackPossible && <Button onClick={this.onBackHandler}>Back</Button>}
         <Row>
           {currentMenu &&
             currentMenu.type === MODULE_TYPE &&
@@ -61,6 +66,10 @@ class Menu extends React.Component<MenuProps> {
     }
     return null;
   };
+  // tslint:disable-next-line: variable-name
+  private onBackHandler = (_event: React.MouseEvent<Button>) => {
+    this.props.setPrevMenuActionCreator();
+  };
 }
 
 /** connect the component to the store */
@@ -68,18 +77,23 @@ class Menu extends React.Component<MenuProps> {
 /** Interface to describe props from mapStateToProps */
 interface DispatchedStateProps {
   currentMenu: MenuItem | null;
+  isBackPossible: boolean;
 }
 
 /** Map props to state  */
 const mapStateToProps = (state: Partial<Store>): DispatchedStateProps => {
   const result = {
     currentMenu: getCurrentMenu(state),
+    isBackPossible: !isPrevMenuEmpty(state),
   };
   return result;
 };
 
 /** map props to actions */
-const mapDispatchToProps = { setMenuItemActionCreator: setMenuItem };
+const mapDispatchToProps = {
+  setMenuItemActionCreator: setMenuItem,
+  setPrevMenuActionCreator: setPrevMenu,
+};
 
 /** connect clientsList to the redux store */
 const ConnectedMenu = connect(
