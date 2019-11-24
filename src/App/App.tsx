83e4a21873c8df9db3d0088d1397f2a96703782f
Nafiz Ahmed <nafiz@mpower-social.com>
2019-11-24 16:46:46 +0600

    Replace connected-private-route with route component

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,19 +1,10 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
-import ConnectedPrivateRoute from '@onaio/connected-private-route';
-import { ConnectedLogout, ConnectedOauthCallback, OauthLogin } from '@onaio/gatekeeper';
 import React, { Component } from 'react';
 import { Redirect, Route, Switch } from 'react-router';
 import { Col, Container, Row } from 'reactstrap';
-import Loading from '../components/page/Loading';
-import { DISABLE_LOGIN_PROTECTION } from '../configs/env';
-import { providers } from '../configs/settings';
-import { LOGIN_URL, LOGOUT_URL } from '../constants';
-import { CLIENT_URL } from '../constants';
-import ConnectedClientList from '../containers/Clients/List';
 import Form from '../containers/Form';
 import Menu from '../containers/Menu';
-import { oAuthUserInfoGetter } from '../helpers/utils';
 import './App.css';
 
 library.add(faUser);
@@ -28,49 +19,12 @@ class App extends Component {
             {/* Production hack. Sets the router to home url on app startup */}
             <span>{window.location.pathname.includes('index.html') && <Redirect to="/" />}</span>
             <Switch>
-              <ConnectedPrivateRoute
-                disableLoginProtection={DISABLE_LOGIN_PROTECTION}
-                exact={true}
-                path="/"
-                component={Menu}
-              />
-              <ConnectedPrivateRoute
-                disableLoginProtection={DISABLE_LOGIN_PROTECTION}
-                exact={true}
-                path="/form/:id"
-                component={Form}
-              />
-              {/* Not related to Bahis project */}
-              <ConnectedPrivateRoute
-                disableLoginProtection={DISABLE_LOGIN_PROTECTION}
-                exact={true}
-                path={CLIENT_URL}
-                component={ConnectedClientList}
-              />
-              {/* tslint:disable jsx-no-lambda */}
-              <Route
-                exact={true}
-                path={LOGIN_URL}
-                render={routeProps => <OauthLogin providers={providers} {...routeProps} />}
-              />
-              <Route
-                exact={true}
-                path="/oauth/callback/:id"
-                render={routeProps => (
-                  <ConnectedOauthCallback
-                    LoadingComponent={Loading}
-                    providers={providers}
-                    oAuthUserInfoGetter={oAuthUserInfoGetter}
-                    {...routeProps}
-                  />
-                )}
-              />
-              <ConnectedPrivateRoute
-                disableLoginProtection={DISABLE_LOGIN_PROTECTION}
-                exact={true}
-                path={LOGOUT_URL}
-                component={ConnectedLogout}
-              />
+              <Route exact={true} path="/">
+                <Menu />
+              </Route>
+              <Route exact={true} path="/form/:id">
+                <Form />
+              </Route>
             </Switch>
           </Col>
         </Row>
