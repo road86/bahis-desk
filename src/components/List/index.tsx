647affc0b1d729bb54a77442465bc5a8a1f8d28a
Nafiz Ahmed <nafiz@mpower-social.com>
2019-11-25 19:23:55 +0600

    Add fetching of definition in List

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 import Filter from '../../containers/Filter';
+import { ipcRenderer } from '../../services/ipcRenderer';
 
 /** interface for Form URL params */
 interface ListURLParams {
@@ -13,12 +14,27 @@ interface ListState {
 }
 
 class List extends React.Component<RouteComponentProps<ListURLParams>, ListState> {
+  constructor(props: any) {
+    super(props);
+    this.state = { filterDefinition: null, tableDefinition: null };
+  }
+  public async componentDidMount() {
+    const { match } = this.props;
+    const listId = match.params.id || '';
+    const { filterDefinition } = await ipcRenderer.sendSync('fetch-list-definition', listId);
+    this.setState({
+      ...this.state,
+      filterDefinition: filterDefinition ? JSON.parse(filterDefinition) : null,
+    });
+  }
   public render() {
+    const { filterDefinition } = this.state;
     return (
       <div>
         <Link to="/">
           <h1>Back</h1>
         </Link>
+        <div> {filterDefinition || ''} </div>
         <Filter />
       </div>
     );
