8f3d3f7b36b5bc4b8e9fb8c34d3e8d12f58143a5
Nafiz Ahmed <nafiz@mpower-social.com>
2019-11-27 11:53:57 +0600

    Add export of FilterItem

--- src/containers/Filter/index.tsx
+++ src/containers/Filter/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import { FILTER_TEXT_TYPE } from './constants';
+import FilterText, { FilterTextItem } from './Text';
 
 interface LabelObj {
   [key: string]: string;
 }
 
-interface FilterItem {
+export interface FilterItem {
   type: string;
   label: LabelObj;
   name: string;
@@ -27,14 +29,26 @@ interface FilterProps {
 
 class Filter extends React.Component<FilterProps> {
   public render() {
+    const { definition } = this.props;
     return (
       <div>
         <Link to="/">
           <h1>Back</h1>
         </Link>
+        {definition.map(filterItem => this.renderTypeEvaluator(filterItem))}
       </div>
     );
   }
+
+  private renderTypeEvaluator = (filterItem: FilterItem) => {
+    switch (filterItem.type) {
+      case FILTER_TEXT_TYPE: {
+        return <FilterText filterItem={filterItem as FilterTextItem} />;
+      }
+      default:
+        return null;
+    }
+  };
 }
 
 export default Filter;
