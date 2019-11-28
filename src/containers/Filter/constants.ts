5c146892bbe7518aa1ca4cdf9855d39482ae6178
Nafiz Ahmed <nafiz@mpower-social.com>
2019-11-28 20:09:55 +0600

    Add Date Filter type

--- src/containers/Filter/constants.ts
+++ src/containers/Filter/constants.ts
@@ -3,3 +3,6 @@ export type FILTER_TEXT_TYPE = typeof FILTER_TEXT_TYPE;
 
 export const FILTER_NUMBER_TYPE = 'number';
 export type FILTER_NUMBER_TYPE = typeof FILTER_NUMBER_TYPE;
+
+export const FILTER_DATE_TYPE = 'date';
+export type FILTER_DATE_TYPE = typeof FILTER_DATE_TYPE;
