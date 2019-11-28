b1d91466bbbfba84d4243faadadb360b89a2b2b8
Nafiz Ahmed <nafiz@mpower-social.com>
2019-11-28 15:50:59 +0600

    Connect Text Filter with store

--- src/containers/Filter/Text/index.tsx
+++ src/containers/Filter/Text/index.tsx
@@ -1,7 +1,17 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 import Select from 'react-select';
 import { FormGroup, Input, Label } from 'reactstrap';
+import { Store } from 'redux';
 import { FilterItem } from '..';
+import {
+  FilterCondition,
+  FilterValue,
+  getFilterCondition,
+  getFilterValue,
+  setConditionValue,
+  setFilterValue,
+} from '../../../store/ducks/filter';
 import { FILTER_TEXT_TYPE } from '../constants';
 import { TEXT_FILTER_OPERATORS } from './constants';
 
@@ -11,6 +21,10 @@ export interface FilterTextItem extends FilterItem {
 
 export interface TextProps {
   filterItem: FilterTextItem;
+  value: FilterValue;
+  condition: FilterCondition;
+  setConditionValueActionCreator: typeof setConditionValue;
+  setFilterValueActionCreator: typeof setFilterValue;
 }
 
 class FilterText extends React.Component<TextProps> {
@@ -19,11 +33,56 @@ class FilterText extends React.Component<TextProps> {
     return (
       <FormGroup>
         <Label>Text</Label>
-        <Select options={TEXT_FILTER_OPERATORS} />
-        <Input type="text" name={filterItem.name} />
+        <Select options={TEXT_FILTER_OPERATORS} onChange={this.handleConditionChange} />
+        <Input type="text" name={filterItem.name} onChange={this.handleValueChange} />
       </FormGroup>
     );
   }
+
+  private handleValueChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const { filterItem } = this.props;
+    this.props.setFilterValueActionCreator(filterItem.name, [event.currentTarget.value]);
+  };
+
+  private handleConditionChange = (selectedOption: any) => {
+    const { filterItem } = this.props;
+    this.props.setConditionValueActionCreator(filterItem.name, selectedOption.value);
+  };
 }
 
-export default FilterText;
+/** connect the component to the store */
+
+/** Interface to describe props from mapStateToProps */
+interface DispatchedStateProps {
+  value: FilterValue;
+  condition: FilterCondition;
+}
+
+/** Interface to describe props from parent */
+interface ParentProps {
+  filterItem: FilterTextItem;
+}
+
+/** Map props to state  */
+const mapStateToProps = (state: Partial<Store>, parentProps: ParentProps): DispatchedStateProps => {
+  const { filterItem } = parentProps;
+  const result = {
+    condition: getFilterCondition(state, filterItem.name),
+    value: getFilterValue(state, filterItem.name),
+  };
+  return result;
+};
+
+/** map props to actions */
+const mapDispatchToProps = {
+  setConditionValueActionCreator: setConditionValue,
+  setFilterValueActionCreator: setFilterValue,
+};
+
+/** connect clientsList to the redux store */
+const ConnectedFilterText = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FilterText);
+
+export default ConnectedFilterText;
