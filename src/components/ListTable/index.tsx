64a5c655d7f01e078ba5a012ff2be5dfc3539528
Nafiz Ahmed <nafiz@mpower-social.com>
2019-12-09 17:15:06 +0600

    Render Column from columnDefinition

--- src/components/ListTable/index.tsx
+++ src/components/ListTable/index.tsx
@@ -15,34 +15,29 @@ export interface ListTableProps {
 
 class ListTable extends React.Component<ListTableProps> {
   public render() {
+    const { columnDefinition } = this.props;
+    const appLanguage = 'English';
     return (
       <Table>
         <thead>
           <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
+            {columnDefinition.map((singleCol: ColumnObj) => (
+              <th key={singleCol.field_name}> {singleCol.label[appLanguage]} </th>
+            ))}
           </tr>
         </thead>
         <tbody>
           <tr>
             <th scope="row">1</th>
             <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
           </tr>
           <tr>
             <th scope="row">2</th>
             <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
           </tr>
           <tr>
             <th scope="row">3</th>
             <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
           </tr>
         </tbody>
       </Table>
