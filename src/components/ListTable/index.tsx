91f04c0105da51ca0d881777f6ae242a418029df
Nafiz Ahmed <nafiz@mpower-social.com>
2019-12-11 14:24:52 +0600

    Fetch table data with filter property

--- src/components/ListTable/index.tsx
+++ src/components/ListTable/index.tsx
@@ -43,6 +43,18 @@ class ListTable extends React.Component<ListTableProps, ListTableState> {
     this.setState({ ...this.state, tableData: response || [], filters });
   }
 
+  public async componentDidUpdate() {
+    const { datasource, filters } = this.props;
+    const stateFilters = this.state.filters;
+    if (filters !== stateFilters) {
+      const response = await ipcRenderer.sendSync(
+        'fetch-query-data',
+        'select * from ' + datasource.query + this.generateSqlWhereClause(filters)
+      );
+      this.setState({ ...this.state, tableData: response || [], filters });
+    }
+  }
+
   public render() {
     const { columnDefinition } = this.props;
     const appLanguage = 'English';
@@ -69,6 +81,16 @@ class ListTable extends React.Component<ListTableProps, ListTableState> {
       </Table>
     );
   }
+
+  private generateSqlWhereClause = (filters: any) => {
+    let sqlWhereClause = '';
+    Object.keys(filters).forEach(filterName => {
+      if (filters[filterName].sql !== '') {
+        sqlWhereClause = `${sqlWhereClause} and ${filters[filterName].sql}`;
+      }
+    });
+    return sqlWhereClause !== '' ? ' where ' + sqlWhereClause.substring(4) : '';
+  };
 }
 
 export default ListTable;
