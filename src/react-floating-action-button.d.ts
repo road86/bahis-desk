151e1e5bd42277b111bcafc14f0b36976beebd73
Nafiz Ahmed <nafiz@mpower-social.com>
2019-12-30 14:07:42 +0600

    Add typings to react-floating-action-button

---
+++ src/react-floating-action-button.d.ts
@@ -0,0 +1,8 @@
+declare module 'react-floating-action-button' {
+  /** interface to describe config */
+  declare const Link: any;
+  declare const Container: any;
+  declare const Button: any;
+  declare const lightColors: any; 
+  declare const darkColors: any;
+}
