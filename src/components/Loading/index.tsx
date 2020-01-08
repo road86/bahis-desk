4f466292ebd7500775c01f51e257b2c1ed0928d1
Nafiz Ahmed <redoyahmed11@gmail.com>
2020-01-08 17:08:28 +0600

    Add startup page

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { Redirect } from 'react-router';
+import Typist from 'react-typist';
+import 'three-dots/dist/three-dots.css';
 import { ipcRenderer } from '../../services/ipcRenderer';
+import './Loading.css';
 
 /** interface for Loading State */
 interface LoadingState {
@@ -15,15 +18,33 @@ class Loading extends React.Component<{}, LoadingState> {
   public async componentDidMount() {
     const response = await ipcRenderer.sendSync('start-app-sync');
     if (response) {
-      this.setState({ isLoadComplete: true });
+      await setTimeout(() => {
+        this.setState({ isLoadComplete: true });
+      }, 5000);
     }
   }
 
   public render() {
     return (
-      <div>
+      <div className="menu-container">
         {this.state.isLoadComplete && <Redirect to="/menu/" />}
-        <div>Loading ... </div>
+        <div className="loader-container">
+          <Typist cursor={{ hideWhenDone: true }}>
+            <span className="loader-title"> BAHIS </span>
+            <br />
+            <span className="loader-subtitle">
+              Welcome
+              <Typist.Backspace count={7} delay={500} />
+              Loading{' '}
+              <span className="blink-one">
+                .
+                <span className="blink-two">
+                  .<span className="blink-three">.</span>
+                </span>
+              </span>
+            </span>
+          </Typist>
+        </div>
       </div>
     );
   }
