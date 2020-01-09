c500bf687571ed6f2dec7cb799c2e226980383fa
Nafiz Ahmed <redoyahmed11@gmail.com>
2020-01-09 18:00:17 +0600

    Connect OrderBy with store and modify props interface accordingly

--- src/containers/ListTable/OrderBy.tsx/index.tsx
+++ src/containers/ListTable/OrderBy.tsx/index.tsx
@@ -1,11 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as React from 'react';
-import { OrderProperty } from '../../../store/ducks/listTable';
+import { connect } from 'react-redux';
+import { Store } from 'redux';
+import { ColumnObj } from '..';
+import { getOrderValue, OrderProperty, setOrderValue } from '../../../store/ducks/listTable';
 
 /** props interface for OrderBy component */
 export interface OrderByProps {
-  order: OrderProperty;
-  setOrderValueActionCreator: any;
+  order: OrderProperty | null;
+  setOrderValueActionCreator: typeof setOrderValue;
+  colDefifinitionObj: ColumnObj;
 }
 
 class OrderBy extends React.Component {
@@ -14,4 +18,35 @@ class OrderBy extends React.Component {
   }
 }
 
-export default OrderBy;
+/** connect the component to the store */
+
+/** Interface to describe props from mapStateToProps */
+interface DispatchedStateProps {
+  order: OrderProperty | null;
+}
+
+/** Interface to describe props from parent */
+interface ParentProps {
+  colDefifinitionObj: ColumnObj;
+}
+
+/** Map props to state  */
+const mapStateToProps = (state: Partial<Store>, parentProps: ParentProps): DispatchedStateProps => {
+  const result = {
+    order: getOrderValue(state, parentProps.colDefifinitionObj.field_name),
+  };
+  return result;
+};
+
+/** map props to actions */
+const mapDispatchToProps = {
+  setOrderValueActionCreator: setOrderValue,
+};
+
+/** connect OrderBy to the redux store */
+const ConnectedOrderBy = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(OrderBy);
+
+export default ConnectedOrderBy;
