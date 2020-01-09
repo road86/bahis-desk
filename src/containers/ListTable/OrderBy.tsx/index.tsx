254532da6a0e7e223da72c551c70d42b926b39fe
Nafiz Ahmed <redoyahmed11@gmail.com>
2020-01-09 17:32:23 +0600

    Add initial OrderBy component

---
+++ src/containers/ListTable/OrderBy.tsx/index.tsx
@@ -0,0 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as React from 'react';
+
+class OrderBy extends React.Component {
+  public render() {
+    return <FontAwesomeIcon icon={['fas', 'arrow-up']} />;
+  }
+}
+
+export default OrderBy;
