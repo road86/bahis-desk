b87bc588dec5b86d048ae024fc8c34567867f42f
nahid <nahid@mpower-social.com>
2021-10-13 17:00:57 +0600

    In form-details page label has been showed instead of value

--- src/components/FormDetails/index.tsx
+++ src/components/FormDetails/index.tsx
@@ -13,6 +13,9 @@ interface DetailsURLParams {
   id: string;
 }
 
+const SELECT_ALL: string = 'select all that apply';
+const SELECT_ONE = 'select one';
+
 
 function FormDetails(props: RouteComponentProps<DetailsURLParams>) {
   const [formData, setFormData] = React.useState<any>([]);
@@ -22,22 +25,22 @@ function FormDetails(props: RouteComponentProps<DetailsURLParams>) {
   const useStyles = makeStyles(listPageStyles(theme));
   const classes = useStyles();
 
-  const comUpdate = async() => {
+  const comUpdate = async () => {
     const { match } = props;
     const listId = match.params.id || '';
     const formData = await ipcRenderer.sendSync('form-details', listId);
     setUserData(formData.formDetails);
-    let {data, form_id} = formData.formDetails;
+    let { data, form_id } = formData.formDetails;
     data = JSON.parse(data);
     const formDefinitionObj = await ipcRenderer.sendSync('fetch-form-definition', form_id);
     if (formDefinitionObj != null) {
-      const { definition, field_names } = formDefinitionObj;
-      createFormKeyValuePair(JSON.parse(definition), JSON.parse(field_names), data);
+      const { definition, field_names, formChoices } = formDefinitionObj;
+      createFormKeyValuePair(JSON.parse(definition), JSON.parse(field_names), data, JSON.parse(formChoices));
     }
     // setFormData(Object.entries(data));
   }
 
-  const createFormKeyValuePair = (definition: any, fieldNames: any, data: any) => {
+  const createFormKeyValuePair = (definition: any, fieldNames: any, data: any, formChoices: any) => {
     const formData: any[] = [];
     const userInput = Object.entries(data);
     fieldNames.forEach((element: any) => {
@@ -47,20 +50,20 @@ function FormDetails(props: RouteComponentProps<DetailsURLParams>) {
         if (exist[0].includes('/')) {
           const fields = exist[0].split('/');
           let children = definition.children
-          for (let i=0;i <= fields.length - 2; i++) {
+          for (let i = 0; i <= fields.length - 2; i++) {
             const groupObj = children.find((obj: any) => obj.name == fields[i]);
             if (groupObj) {
               children = groupObj.children;
             }
           }
-          formField = children.find((obj: any) => obj.name == fields[fields.length-1]);
+          formField = children.find((obj: any) => obj.name == fields[fields.length - 1]);
         } else {
           formField = definition.children.find((obj: any) => obj.name == exist[0]);
         }
         if (formField) {
           formData.push({
             label: formField.label,
-            value: typeof exist[1] == 'string' ? exist[1] : JSON.stringify(exist[1]),
+            value: getReadableValue(exist[1], formField, formChoices),
           })
           console.log(formField.label, exist[1]);
         }
@@ -69,23 +72,58 @@ function FormDetails(props: RouteComponentProps<DetailsURLParams>) {
     setFormData(formData);
   }
 
-  React.useEffect(()=> {
-      comUpdate();
-  }, [])
+  const getReadableValue = (fieldValue: any, formField: any, formChoices: any) => {
+
+    //  it means that value has been selected from csv-list.
+    if (formField && formField.control && formField.control.appearance && formField.control.appearance.includes('search')) {
+      const processedStringArray = formField.control.appearance.match(
+        /\([^\)]+\)/i
+      ) || [''];
+      let params = processedStringArray[0];
+
+      if (params.length > 2) {
+        params = params.substring(1, params.length - 1);
+        const csvName = params.split(',')[0].replaceAll('\'', '');
   
+        const choices = formChoices[`${csvName}.csv`];
+        let result = choices.find((option: any)=> option[formField.children[0].name] === fieldValue);
+        result = result[formField.children[0].label['English']];
+        console.log('--------csvname :', csvName, result);
+        return result;
+      }
+    } 
+    else if(formField.type === SELECT_ONE) {
+      const result = formField.children.find((option: any)=> option.name === fieldValue);
+      return  typeof result.label == 'string' ? result.label : result.label.English;
+    } 
+    else if(formField.type === SELECT_ALL) {
+      return formField.children
+              .filter((option: any)=> fieldValue.includes(option.name))
+              .map((option: any)=> option.label == 'string' ? option.label : option.label.English)
+              .join(', ');
+    }
+    else {
+      return typeof fieldValue == 'string' ? fieldValue : JSON.stringify(fieldValue);
+    }
+  }
+
+  React.useEffect(() => {
+    comUpdate();
+  }, [])
+
   return (
     <div style={{ marginBottom: 20 }}>
-      <hr className={classes.hrTag}/>
+      <hr className={classes.hrTag} />
       <div style={{ textAlign: 'center' }}>
         <h3 className={classes.header}> Submitted Form Details </h3>
       </div>
-      <hr className={classes.hrTag}/>
+      <hr className={classes.hrTag} />
       <Accordion defaultExpanded>
-        <AccordionSummary  expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-            >
-                User Details
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          User Details
         </AccordionSummary>
         <AccordionDetails style={{ display: 'contents' }}>
           <div style={{ padding: 15 }}>
@@ -107,13 +145,13 @@ function FormDetails(props: RouteComponentProps<DetailsURLParams>) {
                 <TableBody>
                   <TableRow>
                     <TableCell key={'col-label-1'} className="initialism text-uppercase text-nowrap">
-                        {userData != null ? userData.submission_date: ''}
-                      </TableCell>
-                      <TableCell key={'col-label-2'} className="initialism text-uppercase text-nowrap">
-                        {userData != null ? userData.submitted_by: ''}
-                      </TableCell>
-                      <TableCell key={'col-label-3'} className="initialism text-uppercase text-nowrap">
-                        Details
+                      {userData != null ? userData.submission_date : ''}
+                    </TableCell>
+                    <TableCell key={'col-label-2'} className="initialism text-uppercase text-nowrap">
+                      {userData != null ? userData.submitted_by : ''}
+                    </TableCell>
+                    <TableCell key={'col-label-3'} className="initialism text-uppercase text-nowrap">
+                      Details
                       </TableCell>
                   </TableRow>
                 </TableBody>
@@ -123,11 +161,11 @@ function FormDetails(props: RouteComponentProps<DetailsURLParams>) {
         </AccordionDetails>
       </Accordion>
       <Accordion defaultExpanded>
-        <AccordionSummary  expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-            >
-                Form Data
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          Form Data
         </AccordionSummary>
         <AccordionDetails style={{ display: 'contents' }}>
           <div style={{ padding: 15 }}>
@@ -144,17 +182,17 @@ function FormDetails(props: RouteComponentProps<DetailsURLParams>) {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {formData.length && formData.map((rowObj:any, index: number) => {
+                  {formData.length && formData.map((rowObj: any, index: number) => {
                     if (rowObj.label) {
                       return (
                         <TableRow>
-                          <TableCell key={'col-label-1' + index} className="text-nowrap"  style={{ verticalAlign: 'baseline'}}>
+                          <TableCell key={'col-label-1' + index} className="text-nowrap" style={{ verticalAlign: 'baseline' }}>
                             {rowObj.label['English']}
                           </TableCell>
-                          <TableCell key={'col-label-2' + index} className="initialism text-uppercase" style={{ wordBreak: 'break-word'}}>
+                          <TableCell key={'col-label-2' + index} className="initialism text-uppercase" style={{ wordBreak: 'break-word' }}>
                             {rowObj.value}
                           </TableCell>
-                        </TableRow>  
+                        </TableRow>
                       )
                     }
                   })}
