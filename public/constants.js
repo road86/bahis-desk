46dcb48eb46bfee879ba6879b34ecb034075eb5b
M Kunde <mikolaj.kundegorski@gmail.com>
2022-05-17 10:46:54 +0600

    switching to dev server

--- public/constants.js
+++ public/constants.js
@@ -1,9 +1,4 @@
-// SERVER URLShttp://dyn-bahis-dev.mpower-social.com:8043/
-// const SERVER_URL = 'http://dyn-bahis-dev.mpower-social.com'; //dev
-const SERVER_URL = 'http://dynamic-bahis.mpower-social.com';
-// const SERVER_URL = 'http://bahis.dls.gov.bd' // Bahis_govt
-// const SERVER_URL = 'http://dyn-bahis-qa.mpower-social.com'; //qa
-// const SERVER_URL = 'http://192.168.23.5:8001';
+const SERVER_URL = '13.213.82.119';
 // TODO Need to update /0/ at the end of DB_TABLES_ENDPOINT DYNAMICALLY
 const DB_TABLES_ENDPOINT = `${SERVER_URL}/bhmodule/core_admin/get/form-config/?/`;
 const APP_DEFINITION_ENDPOINT = `${SERVER_URL}/bhmodule/core_admin/get-api/module-list/`;
--- src/App/constant.ts
+++ src/App/constant.ts
@@ -1,4 +1,5 @@
-export const SERVER_URL = 'http://dyn-bahis-dev.mpower-social.com:8043';
+export const SERVER_URL = '13.213.82.119';
+//
 // export const SERVER_URL = 'http://192.168.19.16:8043'; // ip of bahis dev server
 // export const SERVER_URL = 'http://192.168.23.24:8001'; //shahin bhai pc
 // export const SERVER_URL = 'http://192.168.19.16:1234';
