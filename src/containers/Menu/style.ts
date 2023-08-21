81c30313a49056a1c3e14c70db8753e81d52f3cb
Chas Nelson <github@chasnelson.co.uk>
2023-08-21 11:39:38 +0100

    feat: add "Latest Improvements" accordian

--- src/containers/Menu/style.ts
+++ src/containers/Menu/style.ts
@@ -3,45 +3,39 @@ import { createStyles, Theme } from '@material-ui/core';
 export const menuStyle = (theme: Theme) =>
   createStyles({
     outerCircle: {
-        height: 220,
-        width: '110%',
-        borderRadius: 10,
-        backgroundColor: theme.palette.info.main,
-        borderWidth: '12px 20px', 
-        border: `solid ${theme.palette.primary.main}`,
-        display: 'grid',
-        alignItems: 'end'
+      aspectRatio: '1/1',
+      width: '100%',
+      border: `20px solid ${theme.palette.primary.main}`,
+      borderRadius: 10,
+      backgroundColor: theme.palette.info.main,
+      display: 'grid',
+      alignItems: 'center',
     },
     circle: {
       margin: 'auto auto',
-      // height: 100,
-      // width: 100,
-      // borderRadius: 50,
-      // borderWidth: 2,
-      // borderStyle: 'solid',
-      // borderColor: theme.palette.primary.main,
-      // backgroundColor: theme.palette.info.main,
       justifyContent: 'center',
-      display: 'flex'
+      display: 'flex',
     },
     innerDiv: {
       width: '100%',
       textAlign: 'center',
-      marginBottom: 20
-      // maxWidth: 'min-content',
-      // verticalAlign: 'middle'
+      marginBottom: 20,
     },
     image: {
       display: 'table-cell',
       verticalAlign: 'middle',
-      color: theme.palette.primary.main
+      color: theme.palette.primary.main,
     },
     iconClass: {
       height: 60,
-      width: 70, 
+      width: 70,
       top: 5,
       left: 5,
-      color: theme.palette.primary.main
+      color: theme.palette.primary.main,
     },
-    labelClass: {}
+    labelClass: {},
+    latestImprovements: {
+      maxHeight: '300px',
+      overflowY: 'scroll',
+    },
   });
