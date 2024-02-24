92e4c0607392a0555ec2eb343ceb4dcb819b6601
Chas Nelson <github@chasnelson.co.uk>
2024-02-24 17:17:13 +0000

    feat: rebuild lists so that they don't need list definitions anymore

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,112 +1,95 @@
-import { makeStyles, Typography, useTheme } from '@material-ui/core';
-import * as React from 'react';
+import { Container, Typography } from '@material-ui/core';
+import { DataGrid, GridColDef } from '@material-ui/data-grid';
+import { useEffect, useState } from 'react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
-import Filter from '../../containers/Filter';
-import ListTable from '../../containers/ListTable';
-import { getNativeLanguageText } from '../../helpers/utils';
-import { ipcRenderer } from '../../services/ipcRenderer';
-import { listPageStyles } from './style';
 import { logger } from '../../helpers/logger';
+import { ipcRenderer } from '../../services/ipcRenderer';
 
-export const FILTER_CHOICES = {
-    sample_select_one: [{ name: 'Dhaka' }, { name: 'Chatrogram' }],
-    sample_select_one_v1: [
-        { name: 'Dhaka', sample_select_one: 'Dhaka' },
-        { name: 'Chatrogram', sample_select_one: 'Chatrogram' },
-    ],
-    sample_select_multiple: [{ name: 'Dhaka' }, { name: 'Chatrogram' }],
-    sample_select_multiple_v1: [
-        { name: 'Dhaka', sample_select_multiple: 'Dhaka' },
-        { name: 'Chatrogram', sample_select_multiple: 'Chatrogram' },
-    ],
-};
-export type FILTER_CHOICES = typeof FILTER_CHOICES;
-
-/** interface for Form URL params */
 interface ListURLParams {
     id: string;
 }
 
-/** interface for List props */
-interface ListProps extends RouteComponentProps<ListURLParams> {
-    appLanguage: string;
-}
+function List(props: RouteComponentProps<ListURLParams>) {
+    const [tableName, setTableName] = useState<string>();
+    const [form, setForm] = useState<any>(null);
+    const [columns, setColumns] = useState<GridColDef[]>([]);
+    const [rows, setRows] = useState<[]>([]);
 
-function List(props: ListProps) {
-    const [columnDefinition, setColumnDefinition] = React.useState<any>(null);
-    const [filterDefinition, setFilterDefinition] = React.useState<any>(null);
-    const [datasource, setDataSource] = React.useState<any>(null);
-    const [filtersValue, setFilterValue] = React.useState<any>({});
-    const [listHeader, setListHeader] = React.useState<{ [key: string]: string }>({});
-    const [listId, setListId] = React.useState<string>('');
-
-    const comUpdate = async () => {
-        const { match } = props as any;
-        const listId = match.params.id || '';
-        logger.info('list id');
-        logger.info(listId);
-        const response = await ipcRenderer.sendSync('fetch-list-definition', listId);
-        //Maybe this wont work with === ?
-        if (response != null || response != undefined) {
-            const { columnDefinition, filterDefinition, datasource, listHeader } = response;
-
-            setDataSource(datasource ? JSON.parse(datasource) : null);
-            setFilterDefinition(filterDefinition ? JSON.parse(filterDefinition) : null);
-            setListHeader(listHeader ? JSON.parse(listHeader) : {});
-            setColumnDefinition(columnDefinition ? JSON.parse(columnDefinition) : null);
-            setListId(listId);
+    const parseFormDefinitionAsColumns = (form: any = null, prefix = '') => {
+        logger.info('parsing form definition as datagrid columns');
+        const mappedColumns = form?.children
+            .filter((field: any) => {
+                if (field.name === 'basic_info') {
+                    return true;
+                } else if (
+                    prefix === 'basic_info_' &&
+                    (field.name === 'division' ||
+                        field.name === 'district' ||
+                        field.name === 'upazila' ||
+                        field.name === 'union' ||
+                        field.name === 'mouza')
+                ) {
+                    return false;
+                } else if (prefix === 'basic_info_') {
+                    return true;
+                } else {
+                    return false;
+                }
+            })
+            .map((field: any) => {
+                if (field.type === 'group') {
+                    return parseFormDefinitionAsColumns(field, `${prefix}${field.name}_`);
+                } else {
+                    return {
+                        field: `${prefix}${field.name}`,
+                        headerName: field.label?.English || field.label?.Bangla || field.label || field.name,
+                        width: 200,
+                        editable: false,
+                    };
+                }
+            });
+        // flatten mapped columns
+        if (mappedColumns) {
+            const flattenedColumns = mappedColumns.flat();
+            logger.info(JSON.stringify(flattenedColumns));
+            return flattenedColumns;
         }
     };
 
-    React.useEffect(() => {
-        comUpdate();
-    }, []);
-
-    const setFiltersValue = (filtersValue: any) => {
-        setFilterValue(filtersValue);
+    const readForm = (form_id: any = null) => {
+        logger.info(`reading definition from forms for form_id: ${form_id}`);
+        const query = `SELECT definition FROM forms WHERE form_id IS ${form_id}`;
+        const response = ipcRenderer.sendSync('fetch-query-data', query);
+        const parsed_response = JSON.parse(response[0]?.definition || '');
+        setForm(parsed_response);
+        setTableName(`bahis_${parsed_response.name}_table`);
     };
 
-    const { appLanguage } = props;
+    const readData = (tableName: any = null) => {
+        logger.info(`reading data from table: ${tableName}`);
+        const query = `SELECT * FROM ${tableName}`;
+        return ipcRenderer.sendSync('fetch-query-data', query);
+    };
 
-    const theme = useTheme();
-    const useStyles = makeStyles(listPageStyles(theme));
-    const classes = useStyles();
+    useEffect(() => {
+        readForm(props.id);
+    }, []);
+
+    useEffect(() => {
+        const data = readData(tableName);
+        setRows(data);
+    }, [tableName, form]);
+
+    useEffect(() => {
+        const parsedColumns = parseFormDefinitionAsColumns(form);
+        setColumns(parsedColumns);
+    }, [form]);
 
     return (
-        <>
-            {columnDefinition && filterDefinition ? (
-                <div>
-                    <hr className={classes.hrTag} />
-                    <div style={{ textAlign: 'center' }}>
-                        <h3 className={classes.header}> {getNativeLanguageText(listHeader, appLanguage)} </h3>
-                    </div>
-                    <hr className={classes.hrTag} />
-                    {filterDefinition && listId !== '' && (
-                        <Filter
-                            definition={filterDefinition}
-                            choices={FILTER_CHOICES}
-                            onSubmitHandler={setFiltersValue}
-                            appLanguage={appLanguage}
-                            listId={listId}
-                            columnDefinition={columnDefinition}
-                            datasource={datasource}
-                        />
-                    )}
-                    {columnDefinition && datasource && (
-                        <ListTable
-                            listId={listId}
-                            columnDefinition={columnDefinition}
-                            datasource={datasource}
-                            filters={filtersValue}
-                        />
-                    )}
-                </div>
-            ) : (
-                <Typography color="secondary" component="h1" variant="h4" align="center" style={{ marginTop: '10%' }}>
-                    Couldn't Find List Definition
-                </Typography>
-            )}
-        </>
+        <Container maxWidth={false} style={{ marginTop: '20px' }}>
+            <Typography variant="h3">{form?.title}</Typography>
+            {columns && rows && <DataGrid columns={columns} rows={rows} logger={logger} pageSize={15} autoHeight />}
+        </Container>
     );
 }
 
