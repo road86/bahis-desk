9c731b5be0c01b48ae07885b8288621fe856b281
Chas Nelson <github@chasnelson.co.uk>
2024-03-08 17:27:15 +0000

    feat: minor additions to theme

--- src/theme.ts
+++ src/theme.ts
@@ -32,5 +32,11 @@ export const theme = createTheme({
         warning: {
             main: '#ff9800',
         },
+        info: {
+            main: '#2196f3',
+        },
+        success: {
+            main: '#4caf50',
+        },
     },
 });
